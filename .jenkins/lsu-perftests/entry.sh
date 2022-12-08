af464696e9eccf700e46fb2cd4b68db601c16314
Panos Syskakis <pansysk75@gmail.com>
2022-12-08 10:41:08 -0600

    Temporarily disable commenting

--- .jenkins/lsu-perftests/entry.sh
+++ .jenkins/lsu-perftests/entry.sh
@@ -52,10 +52,10 @@ cat jenkins-hpx-${configuration_name}.err
 # Get build status
 status_file="jenkins-hpx-${configuration_name}-ctest-status.txt"
 
-# Comment on the PR if any failures
-if [[ $(cat ${status_file}) != 0 ]]; then
-    ./.jenkins/lsu-perftests/comment_github.sh
-fi
+# # Comment on the PR if any failures
+# if [[ $(cat ${status_file}) != 0 ]]; then
+#     ./.jenkins/lsu-perftests/comment_github.sh
+# fi
 
 set -e
 exit $(cat ${status_file})
